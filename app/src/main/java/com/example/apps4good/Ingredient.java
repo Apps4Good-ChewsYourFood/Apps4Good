25a7fbec527c91235a59a0ad7b2afa59f16eb717
shulinglin <126716224+shulinglin@users.noreply.github.com>
2023-04-07 14:47:26 -0400

    Update Ingredient.java

--- app/src/main/java/com/example/apps4good/Ingredient.java
+++ app/src/main/java/com/example/apps4good/Ingredient.java
@@ -23,39 +23,29 @@ public class Ingredient {
         this.name = name;
     }
 
-    /**
-     * Returns the name of the ingredient a possible extension would be to add more
-     * data to the ingredient class. Then we would have to decide if the toString()
-     * includes this data.
-     *
-     * @return - the name of the ingredient
-     */
-    @Override
-    public String toString() {
-        return name;
-    }
+  /**
+	 * Returns the name of the ingredient a possible extension would be to add more
+	 * data to the ingredient class. Then we would have to decide if the toString()
+	 * includes this data.
+	 *
+	 * @return - the name of the ingredient
+	 */
+	@Override
+	public String toString() {
+		return name;
+	}
 
-    /**
-     * Returns the name of the Ingredient
-     * @return - the name of the ingredient
-     */
-    @Override
-    public String toString() {
-        return name;
-    }
-
-    /*
-     * Checks if two Ingredients are the same
-     *
-     * @param check - the Ingredient the method is comparing this Ingredient to
-     * @return - true if the Ingredient objects are the same; false if the objects
-     *         are different
-     */
-    public boolean equals(Ingredient check) {
-        if (name.equals(check.toString()))
-            return true;
-        return false;
-    }
+	/**
+	 * Checks if two Ingredients are the same
+	 * 
+	 * @param check - the Ingredient the method is comparing this Ingredient to
+	 * @return - true if the Ingredient objects are the same; false if the objects
+	 *         are different
+	 */
+	public boolean equals(Ingredient check) {
+		if (name.equals(check.toString()))
+			return true;
+		return false;
+	}
 
 }
-
