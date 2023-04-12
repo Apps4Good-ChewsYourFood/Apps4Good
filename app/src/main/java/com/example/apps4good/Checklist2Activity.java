419d5e9db429c6c09ec906823eb6fe6942cf8f22
vaakash1 <vvaratharajan@wpi.edu>
2023-04-11 22:02:30 -0400

    Prepping for implementation of button functionality (addition of Checklist1Activity, Checklist2Activity, & SwiperActivity.java) and replacing strings with string resources. checklist1.xml is functional on my mobile device.

---
+++ app/src/main/java/com/example/apps4good/Checklist2Activity.java
@@ -0,0 +1,14 @@
+package com.example.apps4good;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Checklist2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checklist2);
+    }
+}
