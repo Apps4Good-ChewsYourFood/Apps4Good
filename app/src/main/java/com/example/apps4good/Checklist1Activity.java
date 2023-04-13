632f7d61602c82e92cc32fdf597df50dbe28c802
vaakash1 <vvaratharajan@wpi.edu>
2023-04-12 21:04:52 -0400

    Version to submit for halfway checkpoint

--- app/src/main/java/com/example/apps4good/Checklist1Activity.java
+++ app/src/main/java/com/example/apps4good/Checklist1Activity.java
@@ -2,7 +2,9 @@ package com.example.apps4good;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Checklist1Activity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class Checklist1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checklist1);
     }
+    public void startMainActivity(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void startChecklist2(View v){
+        Intent intent = new Intent(this, Checklist2Activity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/apps4good/Checklist2Activity.java
+++ app/src/main/java/com/example/apps4good/Checklist2Activity.java
@@ -2,7 +2,9 @@ package com.example.apps4good;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Checklist2Activity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class Checklist2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checklist2);
     }
+
+    public void startChecklist1(View v){
+        Intent intent = new Intent(this, Checklist1Activity.class);
+        startActivity(intent);
+    }
+
 }
