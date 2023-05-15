18c3d726551461305d6ac1819ec32496517e4803
shulinglin <126716224+shulinglin@users.noreply.github.com>
2023-05-15 09:35:05 -0400

    Update SwiperActivity.java

--- app/src/main/java/com/example/apps4good/SwiperActivity.java
+++ app/src/main/java/com/example/apps4good/SwiperActivity.java
@@ -9,6 +9,6 @@ public class SwiperActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.swiper);
+        setContentView(R.layout.swiperingredientonly);
     }
 }
